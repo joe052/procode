ca446d2190709fe84a5b1cb58ef74172caccdda0
joe052 <joekaranjasenior52@gmail.com>
2024-10-19 16:02:01 +0300

    message

--- leetcode/TwoSum.js
+++ leetcode/TwoSum.js
@@ -3,4 +3,31 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+
+let nums = [8, 3, 1, 12, 14, 4, 8];
+let target = 13;
+
+const twoSum = function (nums, target) {
+  let output = [];
+
+  outerLoop: for (let i = 0; i < nums.length; i++) {
+    /**Pick the number you are at */
+    const cursor = nums[i];
+
+    /**ADD THE NUMBER TO ANY OTHER NUMBER IN THE ARRAY OTHER THAN ITSELF
+     *  AND TRY TO FIND IF IT MAYTCHES THE TARGET */
+    for (let j = 0; j < nums.length; j++) {
+      if (i == j) continue;
+      console.log(j);
+      const result = cursor + nums[j];
+      if (result == target) {
+        output.push(i, j);
+        break outerLoop;
+      }
+    }
+  }
+  return output;
+};
+
+const result = twoSum(nums, target);
+console.log(result);
